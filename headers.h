ccf3402e69abe84387a6a81ac7b69d13d2c95cc5
miagrace06 <mcampanella@umail.ucsb.edu>
2024-12-05 09:37:20 -0800

    Update and rename headers_skeleton.h to headers.h

---
+++ headers.h
@@ -0,0 +1,26 @@
+// file: headers.h for newanagrams
+
+// Define class AString here!
+// See PDF for details
+using namespace std;
+class AString {
+private:
+    string StringValue;
+public: 
+    //Constructors
+    AString();
+    AString(string value);
+
+    //Accessors
+    string getStringValue() const;
+
+    //Mutators
+    void cleanUp();
+    void countLetters(int letterCount[]);
+    void GetAString();
+
+};
+
+// DO NOTE CHANGE THESE AND DO NOT ADD TO THEM!
+bool compareCounts(int a[], int b[]);
+const int ARRAYSIZE = 26;
